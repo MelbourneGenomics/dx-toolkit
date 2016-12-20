178287eb3bffc8ca595c83d51eb4ec933795ea3e
Kurt Jensen <kjensen@dnanexus.com>
2016-12-19 16:15:32 -0800

    PTFM-18839 Make python deps and source environment outside python tests

--- build/run_python_integration_tests.py
+++ build/run_python_integration_tests.py
@@ -44,8 +44,8 @@ os.environ['DNANEXUS_INSTALL_PYTHON_TEST_DEPS'] = 'yes'
 def run():
     # src_libs is to ensure that dx-unpack is runnable. If we had "bash unit
     # tests" that were broken out separately, that would obviate this though.
-    subprocess.check_call(["make", "python", "src_libs"], cwd=TOOLKIT_ROOT_DIR)
-    subprocess.check_call(["source {0}".format(ENVIRONMENT_PATH)], shell=True)
+    #subprocess.check_call(["make", "python", "src_libs"], cwd=TOOLKIT_ROOT_DIR)
+    #subprocess.check_call(["source {0}".format(ENVIRONMENT_PATH)], shell=True)
 
     # Somewhat hacky-- ensures that all subprocess calls to dx-* tools
     # load the coverage instrumentation so that their use of dxpy is
